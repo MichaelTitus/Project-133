9ec991691c5ce2b74fe37571c1c9cdf02c9b1e91
MichaelTitus <76416268+MichaelTitus@users.noreply.github.com>
2021-08-06 13:00:09 -0500

    Add files via upload

---
+++ Living_room.js
@@ -0,0 +1,48 @@
+object=[]
+status = "";
+function preload() {
+    img = loadImage('Living_room.JPG')
+}
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center()
+    objectdetector = ml5.objectDetector('cocossd', modelloaded)
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+    
+}
+function draw() {
+    image(img, 0, 0, 640, 420)
+    if (status != "") {
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object detected"
+            fill("green")
+            percent = floor(object[i].confidence * 100)
+            console.log(percent)
+            text(object[i].label + " " + percent + "%", object[i].x,object[i].y)
+            noFill()
+            stroke("blue")
+            rect(object[i].x, object[i].y, object[i].width, object[i].height)
+        }
+    }
+}
+
+function modelloaded() {
+    console.log("model is loaded")
+    status = true;
+    objectdetector.detect(img, gotresult)
+}
+
+function gotresult(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results)
+    object=results
+    }
+    
+}
+
+function homescreen(){
+    window.location="index.html"
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+function livingscreen(){
+    window.location="Living_room.html"
+}
+function bedroomscreen(){
+    window.location="Bedroom.html"
+}
+function myroomscreen(){
+    window.location="My_room.html"
+}
+function kitchenscreen(){
+    window.location="Kitchen.html"
+}
